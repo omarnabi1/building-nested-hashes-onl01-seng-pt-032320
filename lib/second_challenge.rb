dbd303e7d90eba07c77b572ea18f92abc459c7cf
Omar Nabi <onabi20@gmail.com>
2020-04-03 04:37:03 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriach => {}
+      :patriach => {},
+      :matriach => {},
+      :hero => {},
+      :hero_friends => {}
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriach => {},
+      :matriach => {},
+      :heroine => {},
+      :heroine_friends => {}
+      }, 
       }}
 
   
